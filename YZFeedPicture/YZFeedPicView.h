15094683c718453f4067eff8706f6a4010b3cb91
Angela527 <374536021@qq.com>
2016-09-22 17:34:04 +0800

    Release 1.0.0.

---
+++ YZFeedPicture/YZFeedPicView.h
@@ -0,0 +1,17 @@
+//
+//  YZFeedPicView.h
+//  MultiselectImage
+//
+//  Created by QiQi on 16/02/21.
+//  Copyright © 2016年 QiQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YZFeedPicView : UIView
+
+@property (strong, nonatomic) UIImageView *imageView;
+
+@property (strong, nonatomic) UIImageView *closeImage;
+
+@end
---
+++ YZFeedPicture/YZFeedPicView.m
@@ -0,0 +1,21 @@
+//
+//  YZFeedPicView.m
+//  MultiselectImage
+//
+//  Created by QiQi on 16/02/21.
+//  Copyright © 2016年 QiQi. All rights reserved.
+//
+
+#import "YZFeedPicView.h"
+
+@implementation YZFeedPicView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
